6343b4323c8aca8d67ead9ced8a92ca592a5dfee
vincent <1341870251@qq.com>
2019-06-10 13:30:24 +0800

    v1.0.1

---
+++ src/main/java/com/core/config/MybatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.core.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by vincent on 2019-06-10
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
