e0eb5ac53474f294b3904ee168d9dd4c2e4c8adf
vincent <1341870251@qq.com>
2019-09-03 23:50:06 +0800

    v1.0.2

--- src/main/java/com/core/controller/BaseController.java
+++ src/main/java/com/core/controller/BaseController.java
@@ -1,7 +1,36 @@
 package com.core.controller;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vincent on 2019-06-09
  */
 public class BaseController {
+
+    public <T> List exportSupport(List<T> list, List<String> fields){
+        try {
+            List<List<Object>> result = new ArrayList<>();
+            Method[] methods = list.get(0).getClass().getMethods();
+            for (T t : list){
+                List<Object> node = new ArrayList<>();
+                for (String field : fields){
+                    for (Method method : methods) {
+                        if (("get" + field).toLowerCase().equals(method.getName().toLowerCase())) {
+                            Object val = method.invoke(t);
+                            node.add(val);
+                            break;
+                        }
+                    }
+                }
+                result.add(node);
+            }
+            return result;
+        } catch (Exception e){
+            throw new RuntimeException();
+        }
+
+    }
+
 }
