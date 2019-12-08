4476615ca08299064b300c034ad6720c969c3a0d
vincent <1341870251@qq.com>
2019-12-08 23:08:27 +0800

    v1.1.0

--- src/main/java/com/core/common/Arith.java
+++ src/main/java/com/core/common/Arith.java
@@ -9,13 +9,10 @@ import java.math.RoundingMode;
  */
 public class Arith {
 
-
     /**
      * 加法
-     *
-     * @param dot     保留小数点
+     * @param dot 保留小数点
      * @param numbers 数字类型
-     * @return
      */
     public static double add(int dot, Number... numbers) {
         return getAirth(dot, ArithType.ADD.getValue(), numbers);
@@ -23,47 +20,29 @@ public class Arith {
 
     /**
      * 减法
-     *
-     * @param dot     保留小数点
+     * @param dot 保留小数点
      * @param numbers 第一个是被减数
-     * @return
      */
     public static double subtract(int dot, Number... numbers) {
-
-
         return getAirth(dot, ArithType.SUBTRACT.getValue(), numbers);
-
-
     }
 
     /**
      * 乘法
-     *
-     * @param dot     保留小数点
+     * @param dot 保留小数点
      * @param numbers 乘法的因子
-     * @return
      */
     public static double multiplys(int dot, Number... numbers) {
-
-
         return getAirth(dot, ArithType.MULTIPLY.getValue(), numbers);
-
-
     }
 
     /**
      * 除法
-     *
-     * @param dot     保留小数点
+     * @param dot 保留小数点
      * @param numbers 除数，第一个是除数
-     * @return
      */
     public static double divides(int dot, Number... numbers) {
-
-
         return getAirth(dot, ArithType.DIVIDE.getValue(), numbers);
-
-
     }
 
     /**
@@ -86,24 +65,17 @@ public class Arith {
 
     /**
      * 综合的运算
-     *
      * @param dot       保留精度
      * @param arithType 运算的类型
      * @param numbers   需要运算的数字
-     * @return
      */
     private static double getAirth(int dot, int arithType, Number... numbers) {
-
         if (numbers == null || numbers.length == 0) {
             return 0.0;
         }
         BigDecimal bigDecimal = new BigDecimal(numbers[0].toString());
-
-
         for (int i = 1; i < numbers.length; i++) {
-
             switch (arithType) {
-
                 case 0:
                     bigDecimal = bigDecimal.add(new BigDecimal(String.valueOf(numbers[i])));
 
@@ -119,14 +91,9 @@ public class Arith {
                     break;
                 default:
                     return 0.00;
-
             }
-
-
         }
-
         return bigDecimal.setScale(dot, RoundingMode.HALF_UP).doubleValue();
-
     }
 
 
@@ -134,7 +101,6 @@ public class Arith {
      * 运算的类型
      */
     private enum ArithType {
-
         ADD("add", 0),
         DIVIDE("divide", 1),
         SUBTRACT("subtract", 2),
@@ -142,25 +108,13 @@ public class Arith {
         @SuppressWarnings("unused")
 		private String key;
         private int value;
-
         ArithType(String key, int value) {
             this.key = key;
             this.value = value;
         }
-
-
         public int getValue() {
             return value;
         }
-
-
     }
 
-
-    public static void main(String[] args) {
-        int i = 2;
-        int j = 3;
-        System.out.println(percentage(i,j));
-
-    }
 }
