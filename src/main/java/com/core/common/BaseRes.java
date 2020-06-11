9f9888026376473006b781d16cdc141334f6434f
luxiaotao1123 <t1341870251@163.com>
2020-06-11 08:54:32 +0800

    #

--- src/main/java/com/core/common/BaseRes.java
+++ src/main/java/com/core/common/BaseRes.java
@@ -5,6 +5,7 @@ public interface BaseRes {
     String OK = "200-操作成功";
     String EMPTY = "201-暂无数据";
     String LIMIT = "202-无权限";
+    String PARAM = "401-参数为空";
     String DENIED = "403-请重新登录";
     String REPEAT = "407-已存在";
     String ERROR = "500-服务器错误";
