4182f7795377ffa822f85b5a33ecc96bbf5d5d36
luxiaotao1123 <t1341870251@163.com>
2020-08-01 14:56:39 +0800

    #

--- src/main/java/com/core/common/BaseRes.java
+++ src/main/java/com/core/common/BaseRes.java
@@ -8,6 +8,7 @@ public interface BaseRes {
     String PARAM = "401-参数为空";
     String DENIED = "403-请重新登录";
     String REPEAT = "407-已存在";
+    String NO_ACTIVATION = "409-请先激活系统";
     String ERROR = "500-服务器错误";
 
 }
